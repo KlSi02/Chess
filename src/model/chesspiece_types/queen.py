8e9ab7e2a789fbf51a2441b4fdf6bfebfa4d93c2
simon <simonkl080@gmail.com>
2023-11-09 18:08:47 +0100

    .

--- src/model/chesspiece_types/queen.py
+++ src/model/chesspiece_types/queen.py
@@ -8,20 +8,20 @@ class Queen(ChessPiece, CommonMovements):
         super().__init__(team)
 
     def possible_movements(self, chessboard):
-        self.position = self.find_position_of_current_char(chessboard)
+        self.find_position_of_current_char(chessboard)
         alpha, numb = self.position
         numb = int(numb)
 
         self.possible_moves_without_knowing_board = []
 
-        self.check_and_append_moves(self, chessboard, alpha, numb, 1, 1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, 1, -1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, -1, -1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, -1, 1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, 1, 0)
-        self.check_and_append_moves(self, chessboard, alpha, numb, -1, 0)
-        self.check_and_append_moves(self, chessboard, alpha, numb, 0, 1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, 0, -1)
+        self.check_and_append_moves(self, chessboard, alpha, numb, 1, 1)  # step up, right
+        self.check_and_append_moves(self, chessboard, alpha, numb, 1, -1)   # step down, right
+        self.check_and_append_moves(self, chessboard, alpha, numb, -1, -1)  # step down, left
+        self.check_and_append_moves(self, chessboard, alpha, numb, -1, 1)   # step up, left
+        self.check_and_append_moves(self, chessboard, alpha, numb, 1, 0)    # step right
+        self.check_and_append_moves(self, chessboard, alpha, numb, -1, 0)   # step left
+        self.check_and_append_moves(self, chessboard, alpha, numb, 0, 1)    # step up
+        self.check_and_append_moves(self, chessboard, alpha, numb, 0, -1)   # step down
 
         return self.possible_moves_without_knowing_board
 
