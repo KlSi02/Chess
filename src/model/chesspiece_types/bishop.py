6fe0eb454275394fcc10bd471ffe9aad4f892ba6
simon <simonkl080@gmail.com>
2023-11-09 18:15:14 +0100

    .

--- src/model/chesspiece_types/bishop.py
+++ src/model/chesspiece_types/bishop.py
@@ -8,15 +8,15 @@ class Bishop(ChessPiece, CommonMovements):
         super().__init__(team)
 
     def possible_movements(self, chessboard):
-        self.position = self.find_position_of_current_char(chessboard)
+        self.find_position_of_current_char(chessboard)
         alpha, numb = self.position
         numb = int(numb)
 
         self.possible_moves_without_knowing_board = []
 
-        self.check_and_append_moves(self, chessboard, alpha, numb, 1, 1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, 1, -1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, -1, -1)
-        self.check_and_append_moves(self, chessboard, alpha, numb, -1, 1)
+        self.check_and_append_moves(self, chessboard, alpha, numb, 1, 1)    # step up, right
+        self.check_and_append_moves(self, chessboard, alpha, numb, 1, -1)   # step down, right
+        self.check_and_append_moves(self, chessboard, alpha, numb, -1, -1)  # step down, left
+        self.check_and_append_moves(self, chessboard, alpha, numb, -1, 1)   # step up, left
 
         return self.possible_moves_without_knowing_board
