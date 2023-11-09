bca960a76537a1d17ed628edf08b7e55d70d70fc
simon <simonkl080@gmail.com>
2023-11-09 18:21:09 +0100

    .

---
+++ tests/chesspiece_types/test_king.py
@@ -0,0 +1,18 @@
+import pytest
+from src.model.chesspiece_types.king import King
+from src.model.chessboard import Chessboard
+from utils.helpers import PieceTeam
+
+
+@pytest.fixture
+def king():
+    black_king = King(PieceTeam.BLACK)
+    return black_king
+
+
+def test_possible_movements(king):
+    chessboard = Chessboard()
+    chessboard.save_chars_and_positions["E5"] = king
+
+    possible_moves = king.possible_movements(chessboard)
+    assert possible_moves == [('E', 6), ('F', 6), ('F', 5), ('F', 4), ('E', 4), ('D', 4), ('D', 5), ('D', 6)]
