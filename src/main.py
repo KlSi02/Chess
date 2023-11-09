bc1f66cd0503875c09fef6dab34fcac5bddcde65
simon <simonkl080@gmail.com>
2023-11-09 15:50:24 +0100

    .

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from view.chessboard import Chessboard
+from view.chessboard import UIChessboard
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Chessboard()
+    window = UIChessboard()
     window.show()
     sys.exit(app.exec())
