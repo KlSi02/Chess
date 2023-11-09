ec4dd2c6795de46dcb1e81f1bd011e0e60a7712f
simon <simonkl080@gmail.com>
2023-11-09 15:58:38 +0100

    .

--- tests/chesspiece_types/test_pawn.py
+++ tests/chesspiece_types/test_pawn.py
@@ -1,33 +1,35 @@
 import pytest
-from engine.engine_character_rules.e_black_pawn import black_pawn, player_black
-from engine.engine_chessboard.e_chessboard import EngineChessboard as Chessboard
-
+from src.model.chesspiece_types.pawn import Pawn
+from src.model.chessboard import Chessboard
+from utils.helpers import PieceTeam
 
 
 @pytest.fixture
-def setup_black_pawn():
+def pawn():
+    pawn = Pawn(PieceTeam.WHITE)
+    return pawn
+
+
+def test_pawn_initialization(pawn):
+    assert pawn.team == PieceTeam.WHITE
+    assert pawn.first_turn is True
+
+
+def test_check_dash_movement(pawn):
     chessboard = Chessboard()
-    pawn = black_pawn()
-    return player_black, chessboard, pawn
+    chessboard.save_chars_and_positions["B3"] = "black"
+    chessboard.save_chars_and_positions["C3"] = "black"
+    chessboard.save_chars_and_positions["A2"] = pawn
+
+    possible_moves = pawn.check_dash_movement(chessboard, "A", 2)
+    assert ("B", "3") in possible_moves
 
 
-def test_check_team(setup_black_pawn):  # passed
-    player_black, chessboard, pawn = setup_black_pawn
-    pawn.check_team()
-    assert pawn.team == "black"
+def test_possible_movements(pawn):
+    chessboard = Chessboard()
+    chessboard.save_chars_and_positions["A2"] = pawn
 
+    possible_moves = pawn.possible_movements(chessboard)
 
-def test_check_dash_movement(setup_black_pawn):  # passed
-    player_black, chessboard, pawn = setup_black_pawn
-    pawn.position = ('A', 7)
-    pawn.check_dash_movement('B', 6)
-    assert pawn.possible_moves == ["B6"]
-    assert player_black.set_checkmate is True
-
-
-def test_possible_movements(setup_black_pawn):  # passed
-    player_black, chessboard, pawn = setup_black_pawn
-    pawn.position = "A7"
-    pawn.first_turn = True
-    pawn.possible_movements()
-    assert pawn.possible_moves == ["B6", "A6", "A5"]
+    assert ("A", 3) in possible_moves
+    assert ("A", 4) in possible_moves
